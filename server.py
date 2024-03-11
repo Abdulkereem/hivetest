dc04bd2d17bf229fa24e4dbff69a7d8b2c20305a
Cyberhero(Ironside) <Abdulkereemokereem@gmail.com>
2024-03-11 10:19:37 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ print("hello world")
 
 @app.route('/')
 def index():
-    return  "Hello World"
+    return  "Hello World, DockHive"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=4400, debug=True)
